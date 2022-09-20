0d703df790a9f43817d330fbd73f236959adc1aa
David Maw <david@maw.name>
2022-09-19 21:32:26 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MAUIListEntry;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+	{
+
+    }
+}
+
